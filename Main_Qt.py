0e524d0bce722e5eef4f714dbce7315f5d92cb3a
Arthur H. Soares <78831683+ArthurHenrique25@users.noreply.github.com>
2022-10-11 14:51:41 -0300

    Main_Qt.py

---
+++ Main_Qt.py
@@ -0,0 +1,21 @@
+ ##### Import Base #####
+ 
+from asyncore import loop
+from importlib.metadata import SelectableGroups
+from timeit import repeat
+from tkinter import Button
+from tkinter.tix import AUTO
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+1 using "venv" = python -m venv env
+
+2 terminal ".\env\Scripts\activate"
+
+3 pip install selenium
+  pip install pyqt5
+
+4 converting ui in py "pyqt5 file_name.ui -o file_name.py -x"
+
+
+
